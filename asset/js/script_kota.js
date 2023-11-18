cb930cf1e558c398b5b23a704bf8ff757c007bd8
kota <koa58550@gmail.com>
2023-11-18 22:54:06 +0900

    kota

---
+++ asset/js/script_kota.js
@@ -0,0 +1,5 @@
+const img_myprofile = document.querySelector("#img_myprofile");
+const click_here = document.querySelector("#click_here");
+click_here.addEventListener("click", function(){
+    img_myprofile.src="./asset/img/a.jpg";
+});
