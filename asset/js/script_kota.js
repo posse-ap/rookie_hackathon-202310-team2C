931ea9301f1fd0139751457362c2bdafe6a6a2f5
kota <koa58550@gmail.com>
2023-11-19 12:27:47 +0900

    こうた完成

--- asset/js/script_kota.js
+++ asset/js/script_kota.js
@@ -1,5 +1,11 @@
 const img_myprofile = document.querySelector("#img_myprofile");
 const click_here = document.querySelector("#click_here");
 click_here.addEventListener("click", function(){
-    img_myprofile.src="./asset/img/a.jpg";
+    img_myprofile.src="./asset/img/kota12.jpg";
+});
+
+const img_myprofile2 = document.querySelector("#img_myprofile2");
+const click_here2 = document.querySelector("#click_here");
+click_here.addEventListener("click",function(){
+    img_myprofile2.src="./asset/img/kota18.jpg";
 });
